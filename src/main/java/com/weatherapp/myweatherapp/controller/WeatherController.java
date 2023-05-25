267c5d4cefc012d83e46d9dcc0ae5cf882542cfa
alanmicah <31675641+alanmicah@users.noreply.github.com>
2023-05-25 16:29:34 +0100

    which city is raining

--- src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
+++ src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
@@ -28,8 +28,9 @@ public class WeatherController {
   @GetMapping("/compare-daylight/{city1}/{city2}")
   public ResponseEntity<String> compareDayLight(@PathVariable("city1") String city1, @PathVariable("city2") String city2) {
 
-    // CityInfo daylightC1 = weatherService.forecastByCity(city1);
-    // CityInfo daylightC2 = weatherService.forecastByCity(city2);
+    CityInfo daylightC1 = weatherService.forecastByCity(city1);
+    CityInfo daylightC2 = weatherService.forecastByCity(city2);
+    
     int daylightC1 = weatherService.compareDayLight(city1);
     int daylightC2 = weatherService.compareDayLight(city2);
     String result;
@@ -44,6 +45,22 @@ public class WeatherController {
 
     return ResponseEntity.ok(result);
   }
+
+  private String longestDay(String city) {
+    CityInfo ci = weatherService.forecastByCity(city); 
+    if (ci != null && ci.CurrentConditions() != null) {
+      String sunriseStr = ci.CurrentConditions().sunrise;
+      String sunsetStr = ci.CurrentConditions().sunset;
+
+      // Convert/extract time from string retrieved in .sunrise and .sunset
+
+      // Then find the difference between the two
+      
+
+    }
+  }
+
+
   // TODO: using Visual Crossing Weather API given two city names, check which city its currently raining in
   @GetMapping("/check-rain/{city1}/{city2}")
   public ResponseEntity<String> checkRain(@PathVariable("city1") String city1, @PathVariable("city2") String city2) {
@@ -51,6 +68,19 @@ public class WeatherController {
     CityInfo ci1 = weatherService.forecastByCity(city1);
     CityInfo ci2 = weatherService.forecastByCity(city2);
 
+    boolean rainCity1 = isRaining(city1);
+    boolean rainCity2 = isRaining(city2);
+
+    String result;
+    if(rainCity1 && rainCity1) {
+      result = "Both cities are raining";
+    } else if(rainCity1) {
+      result = city1;
+    } else if (rainCity2) {
+      result = city2;
+    } else {
+      result = "Both cities are not raining";
+    }
     // CityInfo.CurrentConditions currentConditionsc1 = ci1.currentConditions;
     // String currentConditions1 = ci1.currentConditions;
     // CityInfo.CurrentConditions currentConditionsc2 = ci2.currentConditions;
@@ -63,13 +93,10 @@ public class WeatherController {
   private boolean isRaining(String city) {
     CityInfo ci = weatherService.forecastByCity(city); 
     if (ci != null && ci.CurrentConditions() != null) {
-      String condition = ci.CurrentConditions().currentConditions();
+      String condition = ci.CurrentConditions().currentConditions;
       return condition.toLowerCase().contains("rain");
     }
     return false;
   }
-
-
-  //https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London%2CUK?unitGroup=metric&key=
 
 }
